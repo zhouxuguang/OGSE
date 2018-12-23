e8eda1e49bf1d7df2ce3d35452a14e5ef34b7762
zhouxuguang <zhouxuguang236@126.com>
2018-12-23 18:27:20 +0800

    添加大地坐标点

---
+++ EarthEngine/core/Geodetic3D.cpp
@@ -0,0 +1,14 @@
+//
+//  Geodetic3D.cpp
+//  earthEngineCore
+//
+//  Created by Zhou,Xuguang on 2018/12/23.
+//  Copyright © 2018年 Zhou,Xuguang. All rights reserved.
+//
+
+#include "Geodetic3D.h"
+
+EARTH_CORE_NAMESPACE_BEGIN
+
+
+EARTH_CORE_NAMESPACE_END
---
+++ EarthEngine/core/Geodetic3D.h
@@ -0,0 +1,58 @@
+//
+//  Geodetic3D.h
+//  earthEngineCore
+//
+//  Created by Zhou,Xuguang on 2018/12/23.
+//  Copyright © 2018年 Zhou,Xuguang. All rights reserved.
+//
+
+#ifndef Geodetic3D_hpp
+#define Geodetic3D_hpp
+
+#include <stdio.h>
+#include "EarthEngineDefine.h"
+
+EARTH_CORE_NAMESPACE_BEGIN
+
+class Geodetic3D
+{
+public:
+    Geodetic3D(double longitude, double latitude, double height)
+    {
+        _longitude = longitude;
+        _latitude = latitude;
+        _height = height;
+    }
+    
+    Geodetic3D(double longitude, double latitude)
+    {
+        _longitude = longitude;
+        _latitude = latitude;
+        _height = 0;
+    }
+    
+    double Longitude()
+    {
+        return _longitude;
+    }
+    
+    double Latitude()
+    {
+        return _latitude;
+    }
+    
+    double Height()
+    {
+        return _height;
+    }
+    
+private:
+    double _longitude;
+    double _latitude;
+    double _height;
+};
+
+
+EARTH_CORE_NAMESPACE_END
+
+#endif /* Geodetic3D_hpp */
